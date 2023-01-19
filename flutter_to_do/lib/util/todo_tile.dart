c50643e96e0ef04d567922a25a66ff4ba6a9971b
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-19 22:12:01 +0300

    8:08

--- flutter_to_do/lib/util/todo_tile.dart
+++ flutter_to_do/lib/util/todo_tile.dart
@@ -3,10 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            //checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+
+            //task name
+            Text(taskName),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
   }
 }
