81a541082aa3c88c7ad80f0ba0143803e3625e95
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-01-20 21:39:56 +0300

    21:19

--- flutter_to_do/lib/util/dialog_box.dart
+++ flutter_to_do/lib/util/dialog_box.dart
@@ -6,15 +6,23 @@ class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.yellow[300],
       content: Container(
           height: 120,
           child: Column(
             children: [
               //get user input
-              TextField(),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "add a new task",
+                ),
+              ),
 
               //save and cancel buttons
+              Row(
+                children: [],
+              ),
             ],
           )),
     );
